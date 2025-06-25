5ba01ea0e3035450875110894e6ddb5ec1e68714
lee <lkm1401@gmail.com>
2025-06-25 17:11:53 +0900

    .md file modified commit~

--- src/chapter03/VariableScopeTest.java
+++ src/chapter03/VariableScopeTest.java
@@ -15,11 +15,22 @@ public class VariableScopeTest {
 	public static void main(String[] args) {
 		//main method 지역 변수
 		int number = 100;
+		String str = new String("홍길동");
+		String str2;  //선언
+		
+		{
+			int number2 = 200;
+			str2 = "홍길순";  //할당
+			System.out.println("지역변수 : " + number2);
+			System.out.println("지역변수 : " + str2);
+		}
 		
 		System.out.println("전역변수 : " + gnumber);
 		System.out.println("전역상수 : " + START);
 		System.out.println("전역상수 : " + END);
 		System.out.println("지역변수 : " + number);
+		System.out.println("참조변수 : " + str);
+		System.out.println("지역변수 : " + str2);
 	}
 }
 
