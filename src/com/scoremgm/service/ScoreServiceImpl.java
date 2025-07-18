bec76adced5c50f2031ae6d9d6babebf38f62529
lee <lkm1401@gmail.com>
2025-07-18 10:11:54 +0900

    2025-07-18 commit~

--- src/com/scoremgm/service/ScoreServiceImpl.java
+++ src/com/scoremgm/service/ScoreServiceImpl.java
@@ -104,7 +104,36 @@ public class ScoreServiceImpl implements ScoreService{
 		sms.selectMenu();
 	}
 	
-	public void search() {}
+	/**
+	 * 검색 : 학번 기준
+	 */
+	@Override
+	public void search() {
+		if(getCount() != 0) {
+			System.out.print("학번> ");
+			String no = scan.next();
+			
+//			List<Member> list = repository.findAll();	
+//			System.out.println("-------------------------------------------------");
+//			System.out.println("학번\t\t\t이름\t\t전공\t국어\t영어\t수학");
+//			System.out.println("-------------------------------------------------");
+//			list.forEach((member) -> {
+//				System.out.print(member.getNo() +" \t");
+//				System.out.print(member.getName() +" \t");
+//				System.out.print(member.getDepartment() +" \t");
+//				System.out.print(member.getKor() +" \t");
+//				System.out.print(member.getEng() +" \t");
+//				System.out.print(member.getMath() +" \n");
+//			});
+//			System.out.println("-------------------------------------------------");
+		} else {
+			System.out.println("=> 등록된 학생이 없습니다. ");
+		}
+		sms.showMenu();
+		sms.selectMenu();		
+	}
+	
+	
 	public void update() {}
 	public void delete() {}
 	
